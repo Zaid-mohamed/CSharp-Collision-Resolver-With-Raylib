535b3844ac15b4db474b1d3ccf5ac12b92c489a1
Zaid Mohamed <zaidmohamedhmd@gmail.com>
2025-07-22 23:40:14 +0300

    dotnet things

--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b87315e8547cc7ae01dd543e12935fe8739eaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6562ef174e501405cd784120c5e4f8e63a5ed703")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
